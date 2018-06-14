9e762db92511dc26d3b6c51085e22e06a348500c
Benjamin Rohel <benjamin@BeniMacbookPro.local>
2018-06-14 07:50:30 -0700

    Add VSIX file

--- example.js
+++ example.js
@@ -2,10 +2,7 @@ var variableOne;
 var variableTwo;
 
 var stringLiteral = 
-/* `This is a String literal
+ `This is a String literal
 with multiline and some "double quotes"
 and some 'single quotes'
-and why not, some ${variableOne} and ${variableTwo}` */
-
-// Generated text
-"This is a String literal\nwith multiline and some \"double quotes\"\nand some 'single quotes'\nand why not, some " + variableOne + " and " + variableTwo;
+and why not, some ${variableOne} and ${variableTwo}` 
